08b6850d3eeadb77395d81d68403c5ac0434ae90
mahdi <mahdibelabbas@gmail.com>
2022-06-06 19:06:12 -0400

    js code added

--- scripts.js
+++ scripts.js
@@ -12,48 +12,50 @@ const left=document.getElementById('left')
 const right=document.getElementById('right')
 
 
-takeoff.addEventListener('click',()=>{
-    if(confirm('Confirm that the shuttle is ready for takeoff')){
-        flightStatus.innerHTML="shuttle in flight"
-        shuttleBackground.style.backgroundColor="blue"
-        shuttleHeight.innerHTML=10000
-        rocket.style.transform='translateY(0px)'
-    }
-})
-land.addEventListener('click',()=>{
-    alert('The shuttle is landing. Landing gear engaged.')
-    flightStatus.innerText='The shuttle has landed'
-    shuttleBackground.style.backgroundColor='green'
-    shuttleHeight.innerHTML=0
-    rocket.style.transform='translateY(250px)'
-})
 
-abort.addEventListener('click',()=>{
-    if(confirm('Confirm that you want to abort the mission.')){
-        flightStatus.innerHTML='Mission aborted'
+window.addEventListener('load', (event) => {
+ 
+    
+    takeoff.addEventListener('click',()=>{
+        if(confirm('Confirm that the shuttle is ready for takeoff')){
+            flightStatus.innerHTML="shuttle in flight"
+            shuttleBackground.style.backgroundColor="blue"
+            shuttleHeight.innerHTML=10000
+            rocket.style.transform='translateY(0px)'
+        }
+    })
+    land.addEventListener('click',()=>{
+        alert('The shuttle is landing. Landing gear engaged.')
+        flightStatus.innerText='The shuttle has landed'
         shuttleBackground.style.backgroundColor='green'
         shuttleHeight.innerHTML=0
         rocket.style.transform='translateY(250px)'
-    }
-})
-up.addEventListener('click',()=>{
-    rocket.style.transform+= 'translateY(-10px)'
-
-})
-down.addEventListener('click',()=>{
-    rocket.style.transform+= 'translateY(10px)'
-
-})
-left.addEventListener('click',()=>{
-    rocket.style.transform+= 'translateX(-10px)'
-
-})
-right.addEventListener('click',()=>{
-    rocket.style.transform+= 'translateX(10px)'
-
-})
-// Remember to pay attention to page loading!
-
-window.addEventListener('load', (event) => {
-    console.log('page is fully loaded');
-  });
+    })
+    
+    abort.addEventListener('click',()=>{
+        if(confirm('Confirm that you want to abort the mission.')){
+            flightStatus.innerHTML='Mission aborted'
+            shuttleBackground.style.backgroundColor='green'
+            shuttleHeight.innerHTML=0
+            rocket.style.transform='translateY(250px)'
+        }
+    })
+    up.addEventListener('click',()=>{
+        rocket.style.transform+= 'translateY(-10px)'
+        
+    })
+    down.addEventListener('click',()=>{
+        rocket.style.transform+= 'translateY(10px)'
+        
+    })
+    left.addEventListener('click',()=>{
+        rocket.style.transform+= 'translateX(-10px)'
+        
+    })
+    right.addEventListener('click',()=>{
+        rocket.style.transform+= 'translateX(10px)'
+        
+    })
+    // Remember to pay attention to page loading!
+    
+});
